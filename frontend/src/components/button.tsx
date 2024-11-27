01df85f7a52ae9d726432524d7344282431dcdb4
Mike Smith <mic-smith@users.noreply.github.com>
2024-11-27 16:50:45 +0000

    FS-73 Add outline button component (#40)

--- frontend/src/components/button.tsx
+++ frontend/src/components/button.tsx
@@ -1,27 +1,47 @@
 import React from 'react';
 import styles from './button.module.css';
+import classNames from 'classnames';
 
 interface ButtonProps {
   text?: string;
   icon?: string;
   disabled?: boolean;
+  isOutline?: boolean;
+  isPressed?: boolean;
   onClick?: () => void;
 }
 
-export const Button = ({ text, icon, disabled, onClick }: ButtonProps) => {
+export const Button = ({
+  text,
+  icon,
+  disabled,
+  isOutline,
+  isPressed,
+  onClick,
+}: ButtonProps) => {
   const isIconOnly = !text && icon;
 
   return (
-    <div className={styles.button_container}>
+    <div
+      className={classNames(styles.button_container, {
+        [styles.outline]: isOutline,
+      })}
+    >
       <button
         disabled={disabled}
-        className={`${styles.button} ${isIconOnly ? styles.iconOnly : styles.withText}`}
+        className={classNames(styles.button, {
+          [styles.iconOnly]: isIconOnly,
+          [styles.withText]: !isIconOnly,
+          [styles.pressed]: isPressed,
+        })}
         onClick={onClick}
       >
         {icon && (
           <img
             src={icon}
-            className={`${styles.iconBaseStyle} ${isIconOnly ? styles.iconOnlyStyle : ''}`}
+            className={classNames(styles.iconBaseStyle, {
+              [styles.iconOnlyStyle]: isIconOnly,
+            })}
           />
         )}
         {text}
